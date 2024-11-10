6709f72bca54574e28293d105e195d3e36cfdb82
MetRyan <101856114+MetRyan@users.noreply.github.com>
2024-11-10 17:18:10 +0700

    Seed-Data

--- KoiFish_Data/DataSeeder.cs
+++ KoiFish_Data/DataSeeder.cs
@@ -53,6 +53,74 @@ namespace KoiFish_Data
                 });
                 await context.SaveChangesAsync();
             }
+              if (!context.Users.Any(u => u.UserName.Equals("admin2")))
+            {
+                var adminId = Guid.NewGuid();
+                var adminUser = new AppUser()
+                {
+                    Id = adminId,
+                    FullName = "Admin Two",
+                    Email = "admin2@gmail.com",
+                    NormalizedEmail = "ADMIN2@GMAIL.COM",
+                    UserName = "admin2",
+                    NormalizedUserName = "ADMIN2",
+                    Status = true,
+                    SecurityStamp = Guid.NewGuid().ToString(),
+                    LockoutEnabled = false,
+                    CreatedAt = DateTime.Now,
+                    EmailConfirmed = true,
+                    BirthYear = 1990
+                };
+                adminUser.PasswordHash = passwordHasher.HashPassword(adminUser, "123As@");
+                await context.Users.AddAsync(adminUser);
+
+                await context.UserRoles.AddAsync(new IdentityUserRole<Guid>()
+                {
+                    RoleId = rootAdminRoleId,
+                    UserId = adminId,
+                });
+        }  var customers = new[]
+            {
+                new { FullName = "Nguyen Van A", Email = "nguyenvana@gmail.com", UserName = "nguyenvana", BirthYear = 1995 },
+                new { FullName = "Tran Thi B", Email = "tranthib@gmail.com", UserName = "tranthib", BirthYear = 1996 },
+                new { FullName = "Le Van C", Email = "levanc@gmail.com", UserName = "levanc", BirthYear = 1997 },
+                new { FullName = "Pham Thi D", Email = "phamthid@gmail.com", UserName = "phamthid", BirthYear = 1998 },
+                new { FullName = "Hoang Van E", Email = "hoangvane@gmail.com", UserName = "hoangvane", BirthYear = 1999 }
+            };
+
+            foreach (var customer in customers)
+            {
+                if (!context.Users.Any(u => u.UserName.Equals(customer.UserName)))
+                {
+                    var customerId = Guid.NewGuid();
+                     var customerRoleId = Guid.NewGuid();
+                    var customerUser = new AppUser()
+                    {
+                        Id = customerId,
+                        FullName = customer.FullName,
+                        Email = customer.Email,
+                        NormalizedEmail = customer.Email.ToUpper(),
+                        UserName = customer.UserName,
+                        NormalizedUserName = customer.UserName.ToUpper(),
+                        Status = true,
+                        SecurityStamp = Guid.NewGuid().ToString(),
+                        LockoutEnabled = false,
+                        CreatedAt = DateTime.Now,
+                        EmailConfirmed = true,
+                        BirthYear = customer.BirthYear
+                    };
+                    customerUser.PasswordHash = passwordHasher.HashPassword(customerUser, "123As@");
+                    await context.Users.AddAsync(customerUser);
+
+                    await context.UserRoles.AddAsync(new IdentityUserRole<Guid>()
+                    {
+                        RoleId = customerRoleId,
+                        UserId = customerId,
+                    });
+                }
+            }
+
+            await context.SaveChangesAsync();
         }
     }
 }
